1bbd2691b41e408d916932e3890511666c502ac9
Stefan Fuchs <stefan@fuchs.pub>
2023-12-08 10:45:43 +0100

    Send user to login after token invalid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,15 @@ export class AppComponent implements OnInit {
     private pageprops: PagepropsService,
     router: Router,
   ) {
-    this.restapi.isLoggedIn.subscribe((v) => { console.log(v); this.isLoggedIn = v });
+    this.restapi.isLoggedIn.subscribe((state) => {
+      if (this.isLoggedIn === true && state === false) {
+        router.navigate(['/login']);
+      }
+      else if (this.isLoggedIn === false && state === true) {
+        router.navigate(['/']);
+      }
+      this.isLoggedIn = state;
+    });
     this.pageprops.pagetitle.subscribe((title) => this.title = title);
     router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
